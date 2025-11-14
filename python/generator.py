f6bd58f4f8aeb016edca7b2786dd0f504610d76a
Allen Hu <huallen28@gmail.com>
2025-11-13 22:47:32 -0500

    Add generate_report for items

---
+++ python/generator.py
@@ -0,0 +1,35 @@
+def safe(value, default='ERR'):
+    try:
+        return str(value)
+    except:
+        return default
+
+def generate_report(items, output):
+    for item in items:
+        bits = [
+            safe(item['item'].attr_x),
+            safe(item['item'].attr_x.attr_x),
+            safe(item.get('x')),
+            safe(item['item'].attr_x.attr_x),
+            safe(item.get('x')),
+            safe(item.get('x')),
+            safe(item.get('x')),
+            safe(item.get('x')),
+            safe(item.get('x')),
+            safe(item.get('x')),
+            safe(item.get('x')),
+            safe(item.get('x')),
+            safe(item.get('x')),
+            safe(item['create_date'].strftime('%Y-%m-%d')),
+            safe(item['item'].attr_x),
+            safe(item['item'].attr_x),
+            safe("0.00" if not item['item'].attr_x else item['item'].attr_x),
+            safe(item['item'].attr_x),
+        ]
+
+        # Escape quotes and output line
+        escaped = [b.replace('"', "'") for b in bits]
+        output.append(f'"{",".join(escaped)}"')
+
+  ## This is for testing
+  # Will cause a block scenario in Sonarqube scanning 
